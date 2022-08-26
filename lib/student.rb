c921d1cd14f1800e02ff097ed1bb1823f9a4eb58
byrne22 <byron.odhiambo@student.moringaschool.com>
2022-08-26 12:10:42 +0300

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
-class Student 
+require_relative "../lib/user.rb"
+
+class Student < User
+
+    attr_accessor :knowledge
+
+    def initialize
+
+        @knowledge =[]
+    end
+
+    def learn(knowledge)
+        @knowledge << knowledge
+    end
 
 end
+student1= Student.new
+puts student1.first_name="Smith"
